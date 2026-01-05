f9ad8281be583c7b667c98cb0aa7e9e1bc2dde66
sumaiyyashaikh8484 <sumaiyyashaikh8484@gmail.com>
2026-01-05 13:58:13 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>3);
+// console.log(2>=3);
+// console.log(2<3);
+// console.log(2==3);
+// console.log(2!=3);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);//null is treated as zero
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+console.log(2 === "2");//compare datatypes
